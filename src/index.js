ae8716a330da429b8031a472ddac3220a5f0ca10
Ubaid Inamdar <68742258+Ubaid-Inamdar@users.noreply.github.com>
2020-12-09 22:00:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 
-const mongoose = require('mongoose');
-const port = 3000
-const app = require('./app');
-//mongoose.connect('mongodb://localhost/testaroo', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+const express = require('express')
+const app = express()
+const port = 8080
+const {newsArticleModel}=require('./connector');
 
-// mongoose.connection.once('open', () =>{
-//     console.log('connection established')
-// }).on('connectionError',(err) =>{
-//     console.log(err);
-// })
+const onePageArticleCount = 10
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/newFeeds',(req,res)=>{
+    let limit=isNaN(req.query.limit) || !req.query.limit ?10:parseInt(req.query.limit);
+    let offset=isNaN(req.query.offset) || !req.query.offset ?0:parseInt(req.query.offset);
+
+    newsArticleModel.find().limit(limit).skip(offset).then((ele)=>{
+        res.send(ele);
+    });
+});
+
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+module.exports = app;
