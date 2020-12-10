12f5be56ea84b560d351589eceb4e1ea07fe7c06
Ubaid Inamdar <68742258+Ubaid-Inamdar@users.noreply.github.com>
2020-12-11 02:36:52 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,42 +14,52 @@ app.use(bodyParser.json())
 // your code goes here
 
 app.get("/mario", (req, res) => {
-    marioModel.find().then((data) => res.json(data)).catch((error) => {
+    marioModel.find()
+    .then((data)=> res.json(data))
+    .catch((error)=>{
         res.status(400).json({"message": error.message});
-    });
-});
-
-app.get("/mario/:id",(req, res) => {
-    marioModel.findById(req.params.id).then((data) => {
-        res.json(data);
-    }).catch((error) => {
-        res.status(400).json({"message": error.message});
-    });
-});
-
-app.post("/mario", (req, res) => {
-    let newMario = new marioModel ({
-        name : req.body.name,
-        weight: req.body.weight
-    }) 
-    newMario.save().then((data) => {
-        res.status(201).json(data);
-    }).catch((error) => {
-        res.status(400).json({"message": 'either name or weight is missing' })
     })
 });
 
+app.get("/mario/:id", (req, res) => {
+    marioModel.findById(req.params.id)
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((error)=>{
+        res.status(400).json({"message": error.message});
+    })});
+
+app.post("/mario", (req, res) => {
+    let newMario = new marioModel({
+        name : req.body.name,
+        weight: req.body.weight
+    })
+    newMario.save()
+    .then((data)=>{
+        res.status(201).json(data);
+    })
+    .catch((error)=>{
+        res.status(400).json({"message": 'either name or weight is missing'});
+    })
+});
+
+
+
 app.patch("/mario/:id", (req, res) => {
-    marioModel.findByIdANDUpdate(req.params.id, rq.body, {new:true})
+    marioModel.findByIdAndUpdate(req.params.id, req.body, {new:true})
     .then(data => res.status(200).json(data))
-    .catch(error => res.status(400).json({"message": error.message}))
+    .catch(err => res.status(400).json({"message" : err.message}))
 })
 
 app.delete("/mario/:id", (req, res) => {
-    marioModel.delete({_id: rq.params.id}).then(data => {
-        return res.status(200).send({"message": 'character deleted'})
-    }).catch(error => res.status(400).json({"message": error.message}))
+    marioModel.deleteOne({_id: req.params.id})
+    .then(data => {
+        return res.status(200).send({"message":"character deleted"})
+    })
+    .catch(err => res.status(400).json({"message":err.message}))
 })
+
 
 
 
